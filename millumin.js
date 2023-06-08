257fb722881abefbbb02023bbaee0cca52cb1b43
Matteo Piccina <matteo.piccina@cvm.it>
2023-06-08 11:32:08 +0200

    Update to Companion v3 module format

--- millumin.js
+++ millumin.js
@@ -1,47 +1,50 @@
-var instance_skel = require('../../instance_skel');
-var actions       = require('./actions');
-var debug;
+import { InstanceBase, InstanceStatus, runEntrypoint } from '@companion-module/base';
+import OSC from 'osc';
+import actions from "./actions.js"
+import configFields from './configFields.js';
 var log;
 let currentColumn;
-var OSC = require('osc');
+const {UDPPort} = OSC;
 
-class instance extends instance_skel {
+class MilluminInstance extends InstanceBase {
 
 	constructor(system,id,config) {
 		super(system,id,config)
 
-		Object.assign(this, {...actions})
+		Object.assign(this, {
+			...actions,
+			...configFields,
+		})
 		this.connecting = false;
 		this.actions()
 	}
 
-	actions(system) {
-		this.setActions(this.getActions(this));
-	}
-
-	updateConfig(config) {
+	configUpdated(config) {
 		this.config = config;
 	}
 
-	init() {
-		debug = this.debug;
-		this.status(this.STATUS_UNKNOWN, "Connecting");
+	async init(config) {
+		this.updateStatus(InstanceStatus.Connecting);
 		log = this.log;
-		this.init_variables()
-		this.init_osc()
+		this.configUpdated(config);
+		this.setActionDefinitions(this.actions())
+		this.initVariables()
+		this.initOsc()
 	}
 
-	init_variables() {
+	initVariables() {
 
 		var variables = [
-			{ name: 'currentColumn', label: 'current column' },
-			{ name: 'remainingTime', label: 'Remaining time of clips' }
+			{ variableId: 'currentColumn', name: 'Current column' },
+			{ variableId: 'remainingTime', name: 'Remaining time of clips' }
 		]
 
 		this.setVariableDefinitions(variables)
 
 	}
-	init_osc() {
+	initOsc() {
+
+		this.log("debug", "initOsc")
 
 		if (this.connecting) {
 			return;
@@ -52,7 +55,7 @@ class instance extends instance_skel {
 		}
 
 		if (this.config.host !== undefined && this.config.port !== undefined) {
-			this.mSocket = new OSC.UDPPort({
+			this.mSocket = new UDPPort({
 				localAddress: "0.0.0.0",
 				localPort: this.config.receivePort,
 				address: this.config.host,
@@ -64,10 +67,9 @@ class instance extends instance_skel {
 			this.mSocket.open();
 
 			this.mSocket.on("error", (err) => {
-				debug("Error", err);
 				this.log('error', "Error: " + err.message);
 				this.connecting = false;
-				this.status(this.STATUS_ERROR, "Can't connect to Millumin");
+				this.updateStatus(InstanceStatus.ConnectionFailure);
 				if (err.code == "ECONNREFUSED") {
 					this.mSocket.removeAllListeners();
 				}
@@ -77,7 +79,7 @@ class instance extends instance_skel {
 				this.log('error', "Connection to Millumin Closed");
 				this.connecting = false;
 				this.mSocket.removeAllListeners();
-				this.status(this.STATUS_WARNING, "CLOSED");
+				this.updateStatus(InstanceStatus.Disconnected);
 
 			});
 
@@ -85,233 +87,31 @@ class instance extends instance_skel {
 				this.ready = true;
 				this.connecting = false;
 				this.log('info',"Connected to Millumin:" + this.config.host);
-				this.status(this.STATUS_OK);
+				this.updateStatus(InstanceStatus.Ok);
 			});
 
 			this.mSocket.on("message", (message) => {
-				// console.log("received : ", message);
 				if (message.address.match('/millumin/board/launchedColumn')) {
 					currentColumn = message.args[0].value
-					this.setVariable('currentColumn', message.args[0].value)
+					this.setVariableValues({currentColumn: message.args[0].value})
 				} else if (message.address.match(`/millumin/layer:${this.config.layer}/media/time`)) {
-					this.setVariable('remainingTime', Math.round(message.args[1].value - message.args[0].value))
+					this.setVariableValues({remainingTime: Math.round(message.args[1].value - message.args[0].value)})
 				} else {
-					debug(message.address, message.args);
+					this.log('debug', message.address, message.args);
 				}
 			});
 		}
-		this.mSocket.on("data",(data)=>{
-			// console.log("Got",data, "from",this.mSocket.options.address);
-		});
-	}
-
-	// Return config fields for web config
-	config_fields() {
-		return [
-			{
-				type: 'textinput',
-				id: 'host',
-				label: 'Target IP',
-				tooltip: 'The IP of the computer running Millumin',
-				width: 6,
-				regex: this.REGEX_IP
-			},
-			{
-				type: 'textinput',
-				id: 'port',
-				label: 'Target Port',
-				width: 4,
-				regex: this.REGEX_PORT
-			},
-			{
-				type: 'textinput',
-				id: 'receivePort',
-				label: 'Receive Port',
-				width: 4,
-				default: '8000',
-				regex: this.REGEX_PORT
-			},
-			{
-				type: 'textinput',
-				id: 'layer',
-				label: 'Track time of layer name',
-				width: 4,
-				default: 'layer 1'
-			}
-		]
 	}
 
 	// When module gets deleted
 	destroy() {
-		debug("destory", this.id);
+		this.log('debug', "destroy", this.id);
 
 		if (this.mSocket) {
 			this.mSocket.close();
 		}
 
 	}
-
-	action(action) {
-		var id = action.action;
-		var arg = {};
-
-		var osc = {
-			'tColumn':              '/millumin/action/LaunchOrStopColumn',
-			'lColumn':              '/millumin/action/launchColumn',
-			'lColumnName':          '/millumin/action/launchColumn',
-			'prevColumn':           '/millumin/action/launchPreviousColumn',
-			'nxtColumn':            '/millumin/action/launchNextColumn',
-			'playTl':               '/millumin/action/playTimeline',
-			'pauseTl':              '/millumin/action/pauseTimeline',
-			'tplayTl':              '/millumin/action/playOrPauseTimeline',
-			'stopAll':              '/millumin/action/stopColumn',
-			'gotoTlSeg':            '/millumin/action/goToTimelineSegment',
-			'normTime':             '/millumin/selectedLayer/media/normalizedTime',
-			'restartMedia':         '/millumin/selectedLayer/startMedia',
-			'pauseMedia':           '/selectedLayer/pauseMedia',
-			'tPlayMedia':           '/millumin/selectedLayer/startOrPauseMedia',
-			'stopMedia':            '/millumin/selectedLayer/stopMedia',
-			'startMediaAtColumn':   '/millumin/selectedLayer/startMedia',
-			'startNamedMedia':      '/millumin/selectedLayer/startMedia',
-			'selectLayer':          '/millumin/action/selectLayer',
-			'cBoard':               '/millumin/action/selectBoard'
-		};
-
-		if (id == 'tColumn')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "i",
-				value: parseInt(action.options.int)
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'lColumn')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "i",
-				value: parseInt(action.options.int)
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'lColumnName')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "s",
-				value: action.options.string
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'gotoTlSeg')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "s",
-				value: "" + action.options.string
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'normTime')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "f",
-				value: parseFloat(action.options.float)
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'startMediaAtColumn')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "i",
-				value: parseInt(action.options.int)
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'startNamedMedia')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "s",
-				value: "" + action.options.string
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'selectLayer')  {
-			arg = {
-				type: "i",
-				value: parseInt(action.options.int)
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-			debug('sending ',osc[id],arg,"to",this.config.host);
-		}
-
-		else if (id == 'cBoard')  {
-			debug('sending ',osc[id],"to",this.config.host);
-			arg = {
-				type: "i",
-				value: parseInt(action.options.int)
-			};
-			this.oscSend(this.config.host, this.config.port, osc[id], [arg]);
-		}
-
-		else if (id == 'playMediaInColumn') {
-			debug('sending ',`/millumin/${action.options.layer}/startMedia`,"to",this.config.host);
-			let argL = {
-				type: "s",
-				value: "" + action.options.layer
-			};
-			arg = {
-				type: "i",
-				value: parseInt(action.options.int)
-			}
-			this.oscSend(this.config.host, this.config.port, '/millumin/action/selectLayer', [argL]);
-			currentColumn = action.options.int
-			this.setVariable('currentColumn', action.options.int)
-			this.oscSend(this.config.host, this.config.port, '/millumin/selectedLayer/startMedia', [arg]);
-			// console.log('returning var ',this.getVariable('currentColumn'))
-		}
-
-		else if (id == 'playMediaInNextColumn') {
-			let argL = {
-				type: "s",
-				value: "" + action.options.layer
-			};
-			// on first time set value
-			currentColumn !== undefined ? currentColumn++ : currentColumn = 1
-			arg = {
-				type: "i",
-				value: parseInt(currentColumn)
-			}
-			this.oscSend(this.config.host, this.config.port, '/millumin/action/selectLayer', [argL]);
-			this.oscSend(this.config.host, this.config.port, '/millumin/selectedLayer/startMedia', [arg]);
-			this.setVariable('currentColumn', currentColumn)
-		}
-
-		else if (id == 'playMediaInPreviousColumn') {
-			let argL = {
-				type: "s",
-				value: "" + action.options.layer
-			}
-			// on first time set value, and make sure no column lower then 1 is selected
-			currentColumn !== undefined || currentColumn > 1 ? currentColumn-- : currentColumn = 1
-			arg = {
-				type: "i",
-				value: parseInt(currentColumn)
-			}
-
-			this.oscSend(this.config.host, this.config.port, '/millumin/action/selectLayer', [argL]);
-			this.oscSend(this.config.host, this.config.port, '/millumin/selectedLayer/startMedia', [arg]);
-			this.setVariable('currentColumn', currentColumn)
-		}
-
-		else if (osc[id] !== undefined) {
-			debug('sending adress only',osc[id],"to",this.config.host);
-			this.oscSend(this.config.host, this.config.port, osc[id], []);
-		}
-	}
 }
-exports = module.exports = instance;
+
+runEntrypoint(MilluminInstance, []);
